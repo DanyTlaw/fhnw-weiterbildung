7868c86838878245d63d19ca66e46a1fe95a8db3
notyves <yps.notter@gmail.com>
2014-10-05 16:54:22 +0200

    Fehler met bootstrap ergenwo... muessi no aluege...

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'pg'
 gem 'sass-rails', '~> 4.0.3'
 # Use bootstrap for basic styling
 gem 'bootstrap-sass', '~> 3.2.0'
+
+gem "select2-rails"
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,60 @@ module ApplicationHelper
       end)
     end
     nil
-  end	
+  end
+
+  # Call this fuction with:
+  # name: string to be displayd as link
+  # f: the form object that contains the fields
+  # association: the association to add
+  # and an arbitrary number of html options for the link
+  #
+  # example: link_to_add_fields "Add Option", f, :options, class: 'btn'
+  def link_to_add_fields(*args)
+    options = args.extract_options!.symbolize_keys
+    association = args.pop
+    f = args.pop
+    name = args.pop
+
+    # create a new object of the given association class
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    # get the associations form fields
+    fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
+      render(association.to_s, f: builder)
+    end 
+
+    #TODO: gsub seems to be the only funciton that escapes the string correctrly, .html_safe or h() are not working here, why?
+    fields = fields.gsub('','')
+    new_id = DateTime.now().strftime("%Y%m%d%H%S%L")
+    options[:id] = "add_fields_#{new_id}"
+    options[:name] = "add_fields"
+    options[:href] = "#"
+    options["data-blueprint-id"] = "blueprint_#{new_id}"
+    options["data-field-id"] = "new_#{association}"
+    link = content_tag("a", name, options)
+    div = tag("div", data: {blueprint: fields}, hidden: true, id: "blueprint_#{new_id}")
+    link + div
+  end
+
+  # Call this fuction with:
+  # name: string to be displayd as link
+  # f: the form object that contains the fields
+  # and an arbitrary number of html options for the link
+  #
+  # example: link_to_remove_fields "Remove Option", f, class: 'btn'
+  def link_to_remove_fields(*args)
+    options = args.extract_options!.symbolize_keys
+    f = args.pop
+    name = args.pop
+
+    destroy_field = f.hidden_field(:_destroy)
+
+    new_id = DateTime.now().strftime("%Y%m%d%H%S%L")
+    options[:id] = "remove_fields_#{new_id}"
+    options[:name] = "remove_fields"
+    options[:href] = "#"
+
+    link = content_tag("a", name, options)
+    destroy_field + link
+  end
 end
