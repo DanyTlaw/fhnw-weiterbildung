c606c265fb942014390159e99ece4bbef9f3c6dc
danytlaw <daniel.herzogfhnw@gmail.com>
2014-12-04 14:34:31 +0100

    CSS und PDF

--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 20141203173607) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "ccounter"
     t.integer  "profilstatus"
+    t.integer  "ccounter"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
